b2cc9feb095d5bbf912afe3dd5025fe7f9e2d180
SuperJokerayo <1072843554@qq.com>
2023-07-11 13:28:28 +0800

    main function

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,10 @@
 
 int main() {
     ReadLine rl;
-    rl.historyLoad("../resource/history.txt");
-    rl.printHistory();
+    while (1) {
+        auto line = rl.editLine("Joker>>");
+        if (-1 == line) break;
+        else if(0 == line) rl.responseLine(rl.getBuf());
+    }
     return 0;
 }
