9f2adf7a08563ec559743c22644d1391019d14c4
SuperJokerayo <1072843554@qq.com>
2023-07-11 15:11:34 +0800

    add text_color settings

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include "readline.h"
 
-int main() {
+int main(int argc, const char** argv) {
     ReadLine rl;
     while (1) {
+        auto text_color = "default";
+        if(argc > 1) {
+            text_color = argv[argc - 1];
+        }
+        rl.setTextColor(text_color);
         auto line = rl.editLine("Joker>>");
         if (-1 == line) break;
         else if(0 == line) rl.responseLine(rl.getBuf());
